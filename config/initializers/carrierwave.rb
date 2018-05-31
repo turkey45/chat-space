d8d3f25dbc2ebfba9d661c21b3ee55d32bac1db4
梅村拓朗 <takuroumemura@umemuratakurounoMacBook-Pro.local>
2018-05-31 13:11:35 +0900

    コミット
    コミット

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'us-east-1'
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'takuro-test'
-  config.asset_host = 'https://s3-ap-northeast-1.console.amazonaws.com/takuro-test'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/takuro-test'
 end
